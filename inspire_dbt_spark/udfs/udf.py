af36119499ae144898a624b0eb462f3f5e52c5c2
RJ Richardson <rrichardson@inspirecleanenergy.com>
2021-09-02 01:18:24 -0700

    add plugin support

--- inspire_dbt_spark/udfs/udf.py
+++ inspire_dbt_spark/udfs/udf.py
@@ -1,16 +1,10 @@
 from dataclasses import dataclass
-from typing import Callable, Dict
+from typing import Callable, Dict, List
 from importlib import metadata
 
 import logging
 
 logger = logging.getLogger(__name__)
-
-plugins = metadata.entry_points()['spark_udf']
-logger.info('Registering Plugins . . .')
-for plugin in plugins:
-    logger.info(f'Found plugin: {plugin.name}: {plugin.value}')
-    plugin.load()
 
 
 @dataclass
@@ -22,6 +16,7 @@ class RegisteredUDF:
 class UDFRegistry:
 
     registry: Dict[str, RegisteredUDF] = dict()
+    registered_plugins: List[str] = []
 
     @classmethod
     def udf(cls, alias: str):
@@ -39,3 +34,12 @@ class UDFRegistry:
         for udf in cls.registry.values():
             logger.info(f'Registering UDF: {udf.alias} [{udf.udf}]')
             spark.udf.register(udf.alias, udf.udf)
+
+
+plugins = metadata.entry_points()['spark_udf']
+logger.info('Registering Plugins . . .')
+for plugin in plugins:
+    if plugin.value not in UDFRegistry.registered_plugins:
+        logger.info(f'Found plugin: {plugin.name}: {plugin.value}')
+        UDFRegistry.registered_plugins.append(plugin.value)
+        plugin.load()
