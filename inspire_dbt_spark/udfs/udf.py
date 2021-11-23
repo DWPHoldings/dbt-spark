014917d452ff1e6ee8c29f6f422c8e56bd289c35
RJ Richardson <rrichardson@inspirecleanenergy.com>
2021-11-23 01:23:15 -0800

    Create Pandas UDFs

--- inspire_dbt_spark/udfs/udf.py
+++ inspire_dbt_spark/udfs/udf.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Callable, Dict, List, Any, Set
 
+import pyspark.sql.pandas.functions as pdf
+
 try:
     from importlib import metadata
 except ImportError:  # for Python<3.8
@@ -16,6 +18,7 @@ class RegisteredUDF:
     alias: str
     udf: Callable
     return_type: Any
+    pandas_udf: bool
 
 
 class UDFRegistry:
@@ -25,16 +28,16 @@ class UDFRegistry:
     _existing_functions: Set[str] = set()
 
     @classmethod
-    def udf(cls, alias: str, return_type: Any = None):
+    def udf(cls, alias: str, return_type: Any = None, pandas_udf: bool = False):
         def wrapper(fn: Callable):
             logger.info(f'Found custom UDF: {alias}')
-            cls.register_udf(alias, fn, return_type)
+            cls.register_udf(alias, fn, return_type, pandas_udf)
             return fn
         return wrapper
 
     @classmethod
-    def register_udf(cls, alias: str, udf: Callable, return_type: Any = None):
-        cls.registry[alias] = RegisteredUDF(alias=alias, udf=udf, return_type=return_type)
+    def register_udf(cls, alias: str, udf: Callable, return_type: Any = None, pandas_udf: bool = False):
+        cls.registry[alias] = RegisteredUDF(alias=alias, udf=udf, return_type=return_type, pandas_udf=pandas_udf)
 
     @classmethod
     def load_plugins(cls):
@@ -52,5 +55,7 @@ class UDFRegistry:
             cls._existing_functions = set([t.name for t in spark.catalog.listFunctions('default')])
         for udf in filter(lambda u: u.alias not in cls._existing_functions, cls.registry.values()):
             logger.info(f'Registering UDF: {udf.alias} [{udf.udf}]')
+            if udf.pandas_udf:
+                pdf.pandas_udf(udf.udf, udf.return_type)
             spark.udf.register(udf.alias, udf.udf, udf.return_type)
             cls._existing_functions.add(udf.alias)
