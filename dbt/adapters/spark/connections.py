7b6badc5c82174b9dbfbfe88d0e799eed67538ce
RJ Richardson <rrichardson@inspirecleanenergy.com>
2021-12-15 01:03:48 -0800

    flip async task

--- dbt/adapters/spark/connections.py
+++ dbt/adapters/spark/connections.py
@@ -296,11 +296,11 @@ def _query_session(session, query):
 
 
 async def _wait_loop(event: threading.Event):
-    time.sleep(120)
+    await asyncio.sleep(120)
     # print a message to log to ensure that airflow knows the job is still running
     while not event.is_set():
         print('SparkSQL Query is running waiting for results . . . ')
-        time.sleep(60)
+        await asyncio.sleep(60)
 
 
 async def _execute_query_main(session, query):
