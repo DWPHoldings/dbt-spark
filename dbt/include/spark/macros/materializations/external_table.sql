0adf4ddd25a55055628a5e1b68c09a1b796c78c4
RJ Richardson <rrichardson@inspirecleanenergy.com>
2022-03-27 00:22:44 -0700

    post hook

--- dbt/include/spark/macros/materializations/external_table.sql
+++ dbt/include/spark/macros/materializations/external_table.sql
@@ -3,18 +3,13 @@
   {% set target_relation = this %}
   {% set existing_relation = load_relation(this) %}
   {% set tmp_relation = config.get('temp_table_name') %}
-  {%- set options = config.get('options') -%}
-  {%- set driver = config.get('driver') -%}
-
-  {% set create_external_view %}
-    CREATE TEMPORARY VIEW {{ tmp_relation_ext }}
-    USING {{ driver }}
-    {{ options_clause() }}
-  {% endset %}
 
   {{ run_hooks(pre_hooks) }}
 
-  {% do run_query(create_table_as(True, tmp_relation, sql)) %}
+  -- build model
+  {% call statement('main') -%}
+    {{ create_table_as(True, tmp_relation, sql) }}
+  {%- endcall %}
 
   {% do persist_docs(target_relation, model) %}
 
